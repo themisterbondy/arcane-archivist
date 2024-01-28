2b71031a545b26085ac1fbaabd2a4bb563735da4
Alexandre Nascimento <alexandre.n@outlook.com>
2024-01-28 15:37:05 -0300

    Add tests for MagicCard creation
    This commit adds unit tests for creating a MagicCard with valid parameters. The new tests ensure that a MagicCard can be created successfully and that additional properties can be added without issues. The .csproj file and global usings have also been updated as required.

---
+++ tests/ArcaneArchivist.WebApi.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using ArcaneArchivist.WebApi.Entity;
+global using ArcaneArchivist.WebApi.Persistence;
+global using ArcaneArchivist.WebApi.Tests.DependencyInjection;
+global using FluentAssertions;
---
+++ tests/ArcaneArchivist.WebApi.Tests/MagicCards/MagicCardTests.cs
@@ -0,0 +1,82 @@
+namespace ArcaneArchivist.WebApi.Tests;
+
+public class MagicCardTests
+{
+    [Fact]
+    public void MagicCard_Creation_WithValidParameters_Succeeds()
+    {
+        // Arrange
+        var id = MagicCardId.New();
+        var name = "Test Card";
+        var type = CardType.Creature;
+        var rarity = CardRarity.Common;
+        var power = 5;
+        var toughness = 5;
+        var manaCost =
+            ManaCost.Create([ManaCostColor.Create(CardColor.Red, 2), ManaCostColor.Create(CardColor.Blue, 1)]);
+        var quantity = 10;
+
+        // Act
+        var card = MagicCard.Create(id, name, type, rarity, power, toughness,
+            manaCost, quantity
+        );
+
+        // Assert
+        card.Should().BeEquivalentTo(new
+        {
+            Id = id,
+            Name = name,
+            Type = type,
+            Rarity = rarity,
+            Power = power,
+            Toughness = toughness,
+            ManaCost = manaCost,
+            Quantity = quantity
+        });
+    }
+
+    [Fact]
+    public void MagicCard_Creation_WithValidParametersAndAddAdditionalProperties_Succeeds()
+    {
+        // Arrange
+        var id = MagicCardId.New();
+        var name = "Test Card";
+        var type = CardType.Creature;
+        var rarity = CardRarity.Common;
+        var power = 5;
+        var toughness = 5;
+        var manaCost = ManaCost.Create([ManaCostColor.Create(CardColor.Red, 2)]);
+        var description = "Test Description";
+        var collection = "Test Collection";
+        var edition = "Test Edition";
+        var quantity = 10;
+        var imageUrl = "https://test.com/image.jpg";
+
+        // Act
+        var card = MagicCard.Create(id, name, type, rarity, power, toughness,
+            manaCost, quantity
+        );
+
+        card.Description = description;
+        card.Collection = collection;
+        card.Edition = edition;
+        card.ImageUrl = imageUrl;
+
+        // Assert
+        card.Should().BeEquivalentTo(new
+        {
+            Id = id,
+            Name = name,
+            Type = type,
+            Rarity = rarity,
+            Power = power,
+            Toughness = toughness,
+            ManaCost = manaCost,
+            Quantity = quantity,
+            Description = description,
+            Collection = collection,
+            Edition = edition,
+            ImageUrl = imageUrl
+        });
+    }
+}
