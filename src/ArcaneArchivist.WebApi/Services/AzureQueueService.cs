f233953c8abfe46d799246551d0f2b7f38781866
Alexandre Nascimento <alexandre.n@outlook.com>
2024-01-28 21:41:04 -0300

    Implement AzureQueueService for message handling
    Introduced an AzureQueueService in the ArcaneArchivist.WebApi project. This service contains methods for asynchronous sending and receiving of serialized messages to and from an Azure storage queue, providing additional robustness and scalability to our messaging system.

---
+++ src/ArcaneArchivist.WebApi/Services/AzureQueueService.cs
@@ -0,0 +1,46 @@
+using ArcaneArchivist.WebApi.Common.Messaging;
+using Azure.Storage.Queues;
+using Newtonsoft.Json;
+
+namespace ArcaneArchivist.WebApi.Services;
+
+public class AzureQueueService(QueueServiceClient queueServiceClient) : IQueue
+{
+    public async Task SendMessageAsync<T>(string queueName, T message)
+    {
+        try
+        {
+            var serializedMessage = JsonConvert.SerializeObject(message);
+            var queueClient = queueServiceClient.GetQueueClient(queueName);
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync(serializedMessage);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error sending message to queue: " + e.Message);
+        }
+    }
+
+    public async Task<T> ReceiveMessageAsync<T>(string queueName)
+    {
+        try
+        {
+            var queueClient = queueServiceClient.GetQueueClient(queueName);
+            var response = await queueClient.ReceiveMessagesAsync();
+            if (response.Value.Length > 0)
+            {
+                var message = response.Value[0];
+                await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+
+                var deserializedMessage = JsonConvert.DeserializeObject<T>(message.MessageText);
+                return deserializedMessage;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error receiving message from queue: " + e.Message);
+        }
+
+        return default;
+    }
+}
